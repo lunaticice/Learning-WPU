50a038b63e73459c7ef5f14a4eb3c3f01b5a9db0
lunatic_ice <lunatic.icee@gmail.com>
2023-01-30 00:52:44 +0700

    done-image-gallery

---
+++ JavaScript DOM/08 - Image Gallery/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+
+container.addEventListener('click', function (e) {
+  // cek apakah yang di-klik adalah thumb
+  if (e.target.className == 'thumb') {
+    jumbo.src = e.target.src;
+    jumbo.classList.add('fade');
+    setTimeout(function () {
+      jumbo.classList.remove('fade');
+    }, 500);
+
+    thumbs.forEach(function (thumb) {
+      // if (thumb.classList.contains('active')) {
+      //   thumb.classList.remove('active');
+      // }
+      thumb.className = 'thumb';
+    });
+
+    e.target.classList.add('active');
+  }
+});
